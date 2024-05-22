f6dbcd013365d7b328a1d15525dce40c14d9e35b
wooihaw <wooihaw.tan@gmail.com>
2024-05-22 19:42:46 +0800

    Initial commit

---
+++ STP2_Ex1/STP2_Ex1.ino
@@ -0,0 +1,32 @@
+// Define the GPIO pins
+________________________
+________________________
+
+bool ledState = LOW; // Variable to hold the LED state
+int buttonState = 0; // variable for storing the push-button status
+
+void setup() {
+  _________________ // initialize the LED pin as an output
+  _________________ // initialize the push-button pin as an input
+}
+
+void loop() {
+  _________________ // read the state of the push-button
+  // If the button is pressed (the button state is HIGH)
+  if (buttonState == ____) {
+    delay(100); // wait 0.1s
+    _________________ // read the state of the push-button
+    // Check if the button is released (the button state is LOW)
+    if (buttonState == ____) {
+      // If LOW, toggle the state of the LED
+      if (ledState == LOW)
+        ___________________ // set ledState to HIGH
+      else
+       ___________________ // set ledState to LOW
+      // Write ledState to the LED
+      digitalWrite(________, _________);
+    }
+  } else {
+_________________ // wait 0.1s
+  }
+}
